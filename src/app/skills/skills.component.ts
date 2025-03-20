faf4fb0b6d419c8e6beff9694a9f246275e38f09
lachlan-robinson <lachy4242@hotmail.com>
2025-03-20 16:02:47 +1100

    test

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,4 +6,46 @@ import { Component } from '@angular/core';
 	templateUrl: './skills.component.html',
 	styleUrl: './skills.component.css',
 })
-export class SkillsComponent {}
+export class SkillsComponent {
+	frontEndSkills = [
+		{ logo: 'html-logo.svg', name: 'HTML', description: 'Markup' },
+		{ logo: 'css-logo.svg', name: 'CSS', description: 'Styling' },
+		{
+			logo: 'tailwind-logo.svg',
+			name: 'Tailwind',
+			description: 'CSS Framework',
+		},
+		{
+			logo: 'bootstrap-logo.svg',
+			name: 'Bootstrap',
+			description: 'CSS Framework',
+		},
+		{
+			logo: 'js-logo.svg',
+			name: 'JavaScript',
+			description: 'Programming Language',
+		},
+		{
+			logo: 'typescript-logo.svg',
+			name: 'TypeScript',
+			description: 'Type-Safe JavaScript',
+		},
+		{
+			logo: 'angular-logo.svg',
+			name: 'Angular',
+			description: 'Front-End Framework',
+		},
+	];
+
+	backEndSkills = [
+		{
+			logo: 'node-logo.svg',
+			name: 'Node.js',
+			description: 'JavaScript Runtime Environment',
+		},
+	];
+
+	tools = [
+		{ logo: 'git-logo.svg', name: 'Git', description: 'Version Control' },
+	];
+}
